9984a9da2d90d2ed9931d7b83f81b94dbb6257aa
Samuel Y <25449200+EveningSt3r@users.noreply.github.com>
2023-02-04 00:18:08 -0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -33,7 +33,7 @@ public class Main {
         altVocab.put("EAST", "E"); // want to type words go ahead I guess, also (non) case sensitivity
         altVocab.put("WEST", "W");
 
-        Global.place = 22;
+        Global.place = 1;
 
     // import from cutscenes file
         try(Scanner scanner = new Scanner(new BufferedReader(new FileReader("cutscenes.txt")))) {
@@ -73,22 +73,25 @@ public class Main {
         JLabel intro1 = new JLabel("Welcome to Lost Sovereign, a text adventure game by Quadsar/EveningSt3r on Github!");
         intro1.setFont(new Font("Serif", Font.BOLD, 32));
         intro1.setForeground(Color.WHITE);
+        intro1.setBackground(Color.DARK_GRAY);
         intro1.setBounds(335, 200, 10000, 100);
         intro.add(intro1);
 
         JLabel intro2 = new JLabel("Directions (Close or minimize this window to start the game): ");
         intro2.setFont(new Font("Serif", Font.BOLD, 22));
         intro2.setBounds(335, 300, 10000, 100);
+        intro2.setBackground(Color.DARK_GRAY);
         intro2.setForeground(Color.WHITE);
         intro.add(intro2);
 
         JTextArea intro3 = new JTextArea();
-        intro3.setText("Click on the provided text field and enter in a valid direction with your keyboard. Directions represent movement of your character. N for north, S for south, etc. Your \n" +
-                "CP (Combat Power) changes depending on what items you get during your adventure. At certain checkpoints you will receieve an encounter. If your CP does not meet\n" +
-                "a threshold you will be forced to restart. If this occurs, click the text field and enter 'N' to continue. This prompt will reappear as a reminder upon death. Entering\n" +
+        intro3.setText("Click on the provided text field and enter in a valid direction with your keyboard. Directions represent movement of your character. \nN for north, S for south, etc. Press 'enter' with your keyboard once a valid direction is inputted. \nYour " +
+                "CP (Combat Power) changes depending on what items you get during your adventure. \nAt certain checkpoints you will receieve an encounter. If your CP does not meet " +
+                "a threshold you will be forced to restart. \nIf this occurs, click the text field and enter 'N' to continue. This prompt will reappear as a reminder upon death. Entering\n" +
                 "N will warp you back to a checkpoint. Only certain paths allow progression, so make sure to explore.");
-        intro3.setFont(new Font("Serif", Font.BOLD, 20));
-        intro3.setBounds(335, 450, 1500, 500);
+        intro3.setEditable(false);
+        intro3.setFont(new Font("Serif", Font.BOLD, 25));
+        intro3.setBounds(285, 450, 2500, 500);
         intro3.setBackground(Color.BLACK);
         intro3.setForeground(Color.WHITE);
         intro.add(intro3);
